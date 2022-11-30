ac2e32ec42545ff563388153c3e994a02b8f0e3b
Mark Tripoli <mgtripoli@comcast.net>
2022-11-30 00:58:53 -0500

    Ghillie Feeds from post feed screen

--- src/components/ghillie-circle/index.tsx
+++ src/components/ghillie-circle/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {TouchableOpacity} from "react-native-gesture-handler";
 import {Text, VStack, HStack, Avatar, Pressable} from "native-base";
+import {colorsVerifyCode} from '../colors';
 
 export interface IGhillieCircle {
     onPress: (ghillieId?: string) => void;
@@ -13,7 +14,7 @@ export interface IGhillieCircle {
 
 export const GhillieCircle = (props: IGhillieCircle) => {
     return (
-        <HStack px={{base: 3, md: 4}} flex={1} justifyContent="space-evenly">
+        <HStack px={{base: 3, md: 4}} justifyContent="space-evenly">
             <TouchableOpacity onPress={() => props.ghillieId ? props.onPress(props.ghillieId) : props.onPress()}>
                 <VStack space={1} alignItems="center">
                     <Pressable>
@@ -21,6 +22,7 @@ export const GhillieCircle = (props: IGhillieCircle) => {
                             width={props.width || 20}
                             height={props.height || 20}
                             borderWidth="2"
+                            borderColor={colorsVerifyCode.secondary}
                             source={
                                 props.image
                                     ? {uri: props.image}
