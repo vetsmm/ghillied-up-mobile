d63f85b704f4085c5610a18bd54f1fa8cbe47256
Mark Tripoli <mgtripoli@comcast.net>
2022-12-10 01:30:50 -0500

    bump version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
     name: "Ghillied Up",
     slug: "ghillied-up",
     owner: "ghilliedup",
-    version: "1.0.6",
+    version: "1.0.7",
     orientation: "portrait",
     icon: "./assets/logos/logo-square.png",
     scheme: "ghilliedup",
